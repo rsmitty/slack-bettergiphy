bb8d15abc019bf9ab030fc52a9e6d8e7feef8c29
Spencer Smith <spencer@solinea.com>
2015-06-25 16:32:08 -0700

    Cleaned up and commented

--- giphybot.py
+++ giphybot.py
@@ -1,33 +1,44 @@
 from flask import Flask, request
 import json
 import urllib2
-import time
 
 app = Flask(__name__)  
+
+##Base URL for Giphy. Embeds rating, limit, and the beta API key.
 giphyurl = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=5&rating=g&q="  
 
+##Slack variables to edit. Token from slash command and hook url from incoming webhook integration.
 slacktoken = "INSERT-SLACK-TOKEN-HERE"
 slackhookurl = "INSERT-SLACK-WEBHOOK-URL-HERE"
 
 @app.route("/", methods=['POST'])
 def root():
+
+  ##Verify that toke is what we expect, if so, retrieve search string and replace space with plus.
+  ##After, we'll query giphy for our search string
   if request.form['token'] == slacktoken:
     searchtext = request.form['text'].replace(" ","+")
     return_string = query_giphy(searchtext)
   else:
     return_string = "Something is broken!"
+
+  ##Return results to Slack incoming webhook.
   req = urllib2.Request(slackhookurl)
   req.add_header('Content-Type', 'application/json')
   response = urllib2.urlopen(req, return_string)
   return ""
 
+##Crafts a full rest query and retrieves the response from giphy. 
+##Passes result to parse_results.
 def query_giphy(searchtext):
   fullurl = giphyurl+searchtext
-  print fullurl
   response = urllib2.urlopen(fullurl).read()
   returnresponse = parse_results(response)
   return returnresponse
 
+##Parse JSON results, up to 5 gifs. Looks for the first one that's < 2MB to return.
+##If none are small enough, it just returns the first, most relevant result
+##That gif will not expand in Slack
 def parse_results(jsonresponse):
   user = request.form['user_name']
   channel = request.form['channel_id']
@@ -38,11 +49,8 @@ def parse_results(jsonresponse):
   while gifiter < 5:
     if int(jsondict['data'][gifiter]['images']['original']['size']) <= 2097152:
       break
-
     gifiter += 1
 
-  ##If all 5 were larger than 2MB, we'll just return the first (most relevant)
-  ##one and not worry about Slack expansion of the gif.
   if gifiter == 5:
     gifiter = 0
   else:  
